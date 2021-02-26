24642897e18d843283bb4aab8acfa948bfca5b54
gogaeva <zhora.evtushenko@ukr.net>
2021-02-26 15:43:31 +0200

    adds outputting the generation time of each harmonic

--- signal_generator.py
+++ signal_generator.py
@@ -6,12 +6,15 @@ def generateSignal(harmonicNum, frequencyLimit, timeIntervalsNum):
     x = [0] * timeIntervalsNum
     omega0 = int(frequencyLimit / harmonicNum)
     for i in range(0, harmonicNum):
+        start = time.perf_counter_ns()
         omega = omega0 * (i + 1)
         A = random.random()
         phi = random.randint(0, omega-1)
         for t in range(0, timeIntervalsNum):
             xt = A * math.sin(omega * t + phi)
             x[t] += xt
+        end = time.perf_counter_ns()
+        print(f"Harmonic #{i} generating time: {(end - start) / 1000} us")
     return x
 
 def complexityTest(harmonicNum):
